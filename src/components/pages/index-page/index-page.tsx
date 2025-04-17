429f383140a198f06dbfe336f5d055d18d9dae1a
white1996 <white1996@gmail.com>
2025-04-17 08:39:59 +0300

    fix(index-page)

--- src/components/pages/index-page/index-page.tsx
+++ src/components/pages/index-page/index-page.tsx
@@ -1,12 +1,7 @@
 import { PageLayout } from "@/components/shared/page-layout/page-layout";
-import { HeroSection } from "@/components/pages/index-page/hero-section/hero-section";
 
 const IndexPage = () => {
-	return (
-		<PageLayout>
-			<HeroSection />
-		</PageLayout>
-	);
+	return <PageLayout>Hero</PageLayout>;
 };
 
 export { IndexPage };
